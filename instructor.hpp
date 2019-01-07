549337471d559727d135dd910046e13296563956
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-06 22:54:24 -0800

    “lab4”

---
+++ instructor.hpp
@@ -0,0 +1,12 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 2019
+** Description:     Describe
+**
+**
+**
+*********************************************************************/
+#ifndef INSTRUCTOR_HPP
+#define INSTRUCTOR_HPP
+
+#endif
---
+++ student.hpp
@@ -0,0 +1,12 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 2019
+** Description:     Describe
+**
+**
+**
+*********************************************************************/
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#endif
